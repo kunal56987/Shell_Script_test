656197d6f8ae6f5e808955f597fc64771ebe13b4
Kunal56987 <kunalsri56987@gmail.com>
2025-03-22 06:52:28 +0000

    done changes by first branch

--- test.sh
+++ test.sh
@@ -24,3 +24,35 @@ do
 		fi
 	fi
 done
+
+while true
+do
+        read -p "Enter 2nd folder name: " folder2_name
+        if [ -z $folder2_name ]
+        then
+		echo "Folder name can not be Empty plz provide some other name"
+
+        elif [ -d $folder2_name ]
+        then
+                echo "folder is alredy Exist plz provide different name"
+        else
+                
+		mkdir $folder2_name
+		for i in {1..2} 
+		do
+			 read -p "Enter $i  file name: " file1_name
+			 if [ -z $file1_name ]
+			 then
+				 echo "file name can not be empty plz provide non empty name"
+			 elif [ -e $folder2_name/$file1_name ]
+			 then
+				  echo "File is alredy exist with this name"
+			  else
+				  touch $folder2_name/$file1_name
+			fi
+		
+		done
+		break
+		
+        fi
+done
